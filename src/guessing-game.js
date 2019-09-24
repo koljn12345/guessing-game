121ccfdfc40b1c60650ee037d02dd99ea24fdf1e
koljn12345 <litovchenko_kolj@mail.ru>
2019-09-24 16:04:20 +0300

    feat:add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.mid=Math.ceil((this.max+this.min)/2);
+        return this.mid;
+ 
     }
 
     lower() {
-
+        this.max=this.mid;
     }
 
     greater() {
-
+        this.min=this.mid;
     }
 }
 
